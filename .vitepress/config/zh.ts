be1f4d3c90dbaa962e0561335b5cc049f780329f
Doge2077 <1002302241@qq.com>
2025-02-13 19:24:37 +0800

    feat:更改目录结构

--- .vitepress/config/zh.ts
+++ .vitepress/config/zh.ts
@@ -1,5 +1,5 @@
 import { defineConfig, type DefaultTheme } from 'vitepress'
-import { forewordIcon, school, tools, backend, frontend, chatgpt, learn_408, hard_worker, run_bag, acm } from '../theme/icons/svg-path'
+import { school, tools, backend, frontend, chatgpt,  hard_worker, run_bag, acm, javaIcon, netIcon, linuxIcon, springIcon, mysqlIcon, redisIcon } from '../theme/icons/svg-path'
 
 export const zh = defineConfig({
     lang: 'zh-Hans',
@@ -52,47 +52,14 @@ export const zh = defineConfig({
 function nav(): DefaultTheme.NavItem[] {
     return [
         { text: '首页', link: '/main' },
-        { text: '名人墙', link: '/wall' },
+        // { text: '名人墙', link: '/wall' },
     ]
 }
 
 function sidebar(): DefaultTheme.SidebarItem[] {
     return [
         {
-            text: '前言',
-            collapsed: true,
-            items: [
-                { text: '入门须知', link: '/前言/入门须知', },
-            ]
-        },
-        {
-            text: forewordIcon + '新手入门',
-            collapsed: true,
-            items: [
-                { text: '河工计算机学院专业介绍', link: '/新手入门/河工计算机学院专业介绍', },
-                { text: '小白如何选择笔记本电脑', link: '/新手入门/小白如何选择笔记本电脑', },
-                { text: '转专业二三事', link: '/新手入门/转专业二三事', },
-            ]
-        },
-        {
-            text: acm + '竞赛经验',
-            collapsed: true,
-            items: [
-                { text: '竞赛介绍', link: '/竞赛经验/竞赛介绍', },
-                { text: '算法竞赛学习指南', link: '/竞赛经验/算法竞赛学习指南', },
-                { text: 'CTF竞赛学习指南', link: '/竞赛经验/CTF竞赛学习指南', },
-            ]
-        },
-        {
-            text: tools + '开发工具',
-            collapsed: true,
-            items: [
-                { text: '科学上网', link: '/开发工具/科学上网', },
-                { text: 'JetBrains全家桶', link: '/开发工具/JetBrains全家桶', },
-            ]
-        },
-        {
-            text: school + '学科课程',
+            text: netIcon + '计网',
             collapsed: true,
             items: [
                 {
@@ -126,7 +93,64 @@ function sidebar(): DefaultTheme.SidebarItem[] {
             ]
         },
         {
-            text: backend + '后端开发',
+            text: linuxIcon + '操作系统',
+            collapsed: true,
+            items: [
+                { text: '入门须知', link: '/前言/入门须知', },
+            ]
+        },
+        {
+            text: javaIcon + 'Java基础',
+            collapsed: true,
+            items: [
+                { text: '入门须知', link: '/前言/入门须知', },
+            ]
+        },
+        {
+            text: acm + '算法基础',
+            collapsed: true,
+            items: [
+                { text: '竞赛介绍', link: '/竞赛经验/竞赛介绍', },
+                { text: '算法竞赛学习指南', link: '/竞赛经验/算法竞赛学习指南', },
+                { text: 'CTF竞赛学习指南', link: '/竞赛经验/CTF竞赛学习指南', },
+            ]
+        },
+        {
+            text: springIcon + 'Spring',
+            collapsed: true,
+            items: [
+                { text: '竞赛介绍', link: '/竞赛经验/竞赛介绍', },
+                { text: '算法竞赛学习指南', link: '/竞赛经验/算法竞赛学习指南', },
+                { text: 'CTF竞赛学习指南', link: '/竞赛经验/CTF竞赛学习指南', },
+            ]
+        },
+        {
+            text: mysqlIcon + 'MySQL',
+            collapsed: true,
+            items: [
+                { text: '竞赛介绍', link: '/竞赛经验/竞赛介绍', },
+                { text: '算法竞赛学习指南', link: '/竞赛经验/算法竞赛学习指南', },
+                { text: 'CTF竞赛学习指南', link: '/竞赛经验/CTF竞赛学习指南', },
+            ]
+        },
+        {
+            text: redisIcon + 'Redis',
+            collapsed: true,
+            items: [
+                { text: '科学上网', link: '/开发工具/科学上网', },
+                { text: 'JetBrains全家桶', link: '/开发工具/JetBrains全家桶', },
+            ]
+        },
+        {
+            text: tools + '中间件',
+            collapsed: true,
+            items: [
+                { text: '科学上网', link: '/开发工具/科学上网', },
+                { text: 'JetBrains全家桶', link: '/开发工具/JetBrains全家桶', },
+            ]
+        },
+        {
+            text: backend + '软件架构',
             collapsed: true,
             items: [
                 {
@@ -149,35 +173,53 @@ function sidebar(): DefaultTheme.SidebarItem[] {
                 }
             ]
         },
-        {
-            text: frontend + '前端开发',
-            collapsed: true,
-            items: [
-                {
-                    text: '前端开发基础',
-                    collapsed: true,
-                    items: [
-                        { text: 'HTML基础', link: '/前端开发/前端开发基础/HTML基础' },
-                        { text: 'CSS基础', link: '/前端开发/前端开发基础/CSS基础' },
-                        { text: 'JavaScript基础', link: '/前端开发/前端开发基础/JavaScript基础' }
-                    ]
-                },
-                {
-                    text: 'Vue技术栈',
-                    collapsed: true,
-                    items: [
-                        { text: 'Vue基础', link: '/前端开发/Vue技术栈/Vue基础' },
-                    ]
-                },
-                {
-                    text: 'React技术栈',
-                    collapsed: true,
-                    items: [
-                        { text: 'React基础', link: '/前端开发/React技术栈/React基础' },
-                    ]
-                }
-            ]
-        },
+        // {
+        //     text: acm + '测试',
+        //     collapsed: true,
+        //     items: [
+        //         { text: '竞赛介绍', link: '/竞赛经验/竞赛介绍', },
+        //         { text: '算法竞赛学习指南', link: '/竞赛经验/算法竞赛学习指南', },
+        //         { text: 'CTF竞赛学习指南', link: '/竞赛经验/CTF竞赛学习指南', },
+        //     ]
+        // },
+        // {
+        //     text: acm + '运维',
+        //     collapsed: true,
+        //     items: [
+        //         { text: '竞赛介绍', link: '/竞赛经验/竞赛介绍', },
+        //         { text: '算法竞赛学习指南', link: '/竞赛经验/算法竞赛学习指南', },
+        //         { text: 'CTF竞赛学习指南', link: '/竞赛经验/CTF竞赛学习指南', },
+        //     ]
+        // },
+        // {
+        //     text: frontend + '前端开发',
+        //     collapsed: true,
+        //     items: [
+        //         {
+        //             text: '前端开发基础',
+        //             collapsed: true,
+        //             items: [
+        //                 { text: 'HTML基础', link: '/前端开发/前端开发基础/HTML基础' },
+        //                 { text: 'CSS基础', link: '/前端开发/前端开发基础/CSS基础' },
+        //                 { text: 'JavaScript基础', link: '/前端开发/前端开发基础/JavaScript基础' }
+        //             ]
+        //         },
+        //         {
+        //             text: 'Vue技术栈',
+        //             collapsed: true,
+        //             items: [
+        //                 { text: 'Vue基础', link: '/前端开发/Vue技术栈/Vue基础' },
+        //             ]
+        //         },
+        //         {
+        //             text: 'React技术栈',
+        //             collapsed: true,
+        //             items: [
+        //                 { text: 'React基础', link: '/前端开发/React技术栈/React基础' },
+        //             ]
+        //         }
+        //     ]
+        // },
         {
             text: chatgpt + '人工智能',
             collapsed: true,
@@ -185,13 +227,13 @@ function sidebar(): DefaultTheme.SidebarItem[] {
                 { text: '机器学习', link: '/人工智能/机器学习', },
             ]
         },
-        {
-            text: learn_408 + '考研经验',
-            collapsed: true,
-            items: [
-                { text: '24届考研上岸211', link: '/考研经验/24届考研上岸211', },
-            ]
-        },
+        // {
+        //     text: learn_408 + '考研经验',
+        //     collapsed: true,
+        //     items: [
+        //         { text: '24届考研上岸211', link: '/考研经验/24届考研上岸211', },
+        //     ]
+        // },
         {
             text: hard_worker + '工作经验',
             collapsed: true,
@@ -206,14 +248,6 @@ function sidebar(): DefaultTheme.SidebarItem[] {
                         { text: '二本如何进大厂', link: '/工作经验/案例分享/二本如何进大厂' },
                     ]
                 },
-            ]
-        },
-        {
-            text: run_bag + '考公留学',
-            collapsed: true,
-            items: [
-                { text: '考公指南', link: '/考公留学/考公指南', },
-                { text: '润学指南', link: '/考公留学/润学指南', },
             ]
         },
     ]
